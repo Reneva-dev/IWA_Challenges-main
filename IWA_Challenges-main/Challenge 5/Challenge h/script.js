d766f44fbeec4e3be3525253321b1712439ac0b2
Reneva Newman <renevanewman7@gmail.com>
2025-07-08 20:05:28 +0300

    added isNan to check digits

--- IWA_Challenges-main/Challenge 5/Challenge h/script.js
+++ IWA_Challenges-main/Challenge 5/Challenge h/script.js
@@ -3,8 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = /^\d+$/.test(primaryPhone)
-const secondaryValid = /^\d+$/.test(secondaryPhone)
+const primaryValid = !Number.isNan(parseInt (primaryPhone))//should return false
+const secondaryValid = !Number.isNan(parseInt (secondaryPhone))//should return true
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid)
