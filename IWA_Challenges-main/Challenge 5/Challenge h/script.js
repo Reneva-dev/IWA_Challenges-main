d2da1af80faadd3dba9eb981132c1993c5f340d3
Reneva Newman <renevanewman7@gmail.com>
2025-07-08 20:15:58 +0300

    corrected NaN

--- IWA_Challenges-main/Challenge 5/Challenge h/script.js
+++ IWA_Challenges-main/Challenge 5/Challenge h/script.js
@@ -3,8 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = !Number.isNan(parseInt (primaryPhone))//should return false
-const secondaryValid = !Number.isNan(parseInt (secondaryPhone))//should return true
+const primaryValid = !Number.isNaN(parseInt (primaryPhone))//should return false
+const secondaryValid = !Number.isNaN(parseInt (secondaryPhone))//should return true
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid)
