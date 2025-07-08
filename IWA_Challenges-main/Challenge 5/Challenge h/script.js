1bb54448748209e728049162f8e7e3e700a2d27b
Reneva Newman <renevanewman7@gmail.com>
2025-07-08 17:34:31 +0300

    Fix phone number validation logic using regex to ensure numerical strings

--- IWA_Challenges-main/Challenge 5/Challenge h/script.js
+++ IWA_Challenges-main/Challenge 5/Challenge h/script.js
@@ -3,10 +3,9 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const isNumeric = str => /^\d+$/.test(str)
-
-const primaryValid = isNumeric(primaryPhone)
-const secondaryValid = isNumeric(secondaryPhone)
+const primaryValid = /^\d+$/.test(primaryPhone)
+const secondaryValid = /^\d+$/.test(secondaryPhone)
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid)
+
